1d1fdee5c7d52a77d12f430b2d52bb84cebf7f5d
Millie8969 <lukamdujakovic@gmail.com>
2026-02-02 14:34:51 +0100

    Old profile card implementation into Render

---
+++ public/scripts/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+const target = document.querySelector('.profileCardWrapper');
+      const sound = document.getElementById('larry'); 
+
+      target.addEventListener('mouseenter', (e) => {
+        e.preventDefault();
+        sound.play();
+      });
+
+      target.addEventListener('mouseleave', () => {
+        sound.pause();
+        sound.currentTime = 0;
+      });
+    });
